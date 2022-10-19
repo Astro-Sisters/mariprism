7c6f3194c76ca327379dd6653640fc083fc64e21
Palma-Bifani <ppalma@das.uchile.cl>
2022-10-19 23:29:10 +0200

    First tests to check how to create colormaps

---
+++ mariprism/main_idea.py
@@ -0,0 +1,31 @@
+'''
+mariprism v1.0
+
+-------------------------------------------
+Aim to create a python package to have beautiful and fancy colormaps for matplotlib.
+
+1. Have a section of previously defined colormaps 
+    - Which format are the color maps saved? 
+    - have an easy implementation to access and use in imshow/similar functions.
+    - +colorblind frendly features
+
+2. Have easy implementation to create own colormaps 
+    - Give input colors and generate personalized map.
+    - 
+
+3. Maybe have cool plots examples
+
+4. Implement sound feature for blind people... (a los 40 zi)
+
+5. ...
+
+-------------------------------------------
+Useful info to proceed:
+https://www.analyticsvidhya.com/blog/2020/09/colormaps-matplotlib/
+
+-------------------------------------------
+Authors: P. Cáceres-Burgos & P. Palma-Bifani
+'''
+
+## Imports
+import numpy as np
